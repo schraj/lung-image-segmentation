e39b7c6f0004d757d9955b804435075964f797a5
Jeremy Schrader <jeschrader@paypal.com>
2023-11-05 18:18:38 -0800

    fix file list

--- src/data/data_utils.py
+++ src/data/data_utils.py
@@ -5,7 +5,8 @@ def get_file_list(path):
     file_list = []
     for root, dirs, files in os.walk(path):
         for file in files:
-            file_list.append(os.path.join(root, file))
+            if ('test.txt' not in file):
+              file_list.append(os.path.join(root, file))
     return file_list
 
 def get_file_name(directory, starting_letter):
